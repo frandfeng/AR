43f9826c4c3743321fb3fc913d4f911c8951c5f3
frandfeng <frandfeng@gmail.com>
2017-10-18 11:47:29 +0800

    对接所有消息

--- AR3DCity/Interface/PWUnityMsgManager.m
+++ AR3DCity/Interface/PWUnityMsgManager.m
@@ -40,12 +40,50 @@ static PWUnityMsgManager *sharedObject = nil;
     NSDictionary *dic = [PWU3DCodec toArrayOrNSDictionary:[str dataUsingEncoding:NSASCIIStringEncoding]];
     if ([[dic allKeys] containsObject:@"method"]) {
         NSString *func = [dic objectForKey:@"method"];
-        if ([func isEqualToString:@"ReqIntelligentFun"]) {
-            
-        } else if ([func isEqualToString:@"ReqGPSState"]) {
+        /**
+         开起定位服务 （OnGPSStateResult是回调函数）
+         M->Call->A:ReqGPSState()->...->A->Call->M:OnGPSStateResult(bool isOpened)
+         
+         IOS（特殊处理）:
+         U3D->IOS：{"method":"ReqGPSState","params":[]}
+         IOS->U3D："Entrance","OnGPSStateResult",{"params":[{"isOpened":true}]}
+         */
+        if ([func isEqualToString:@"ReqGPSState"]) {
             [self sendMsg2UnityOfType:@"OnGPSStateResult" andValue:@"{\"params\":{\"isOpened\":true}}"];
         }
+        /**
+         GPS定位（OnGPSInfoResult是回调函数）
+         
+         M->Call->A:ReqGPSInfo()->...->A->Call->M:OnGPSInfoResult(float longitude, float latitude)
+         
+         IOS（特殊处理）:
+         U3D->IOS：{"method":"ReqGPSState","params":[]}
+         IOS->U3D："Entrance","OnGPSStateResult",{"params":[{"longitude":10.1},{"latitude":10.1}]}
+         */
+        else if ([func isEqualToString:@"ReqGPSInfo"]) {
+            [self sendMsg2UnityOfType:@"OnGPSInfoResult" andValue:@"{\"params\":{\"longitude\":10.2,\"latitude\":10.1}}"];
+        }
+        /**
+         打电话
+         M->Call->A:ReqCallPhone(string phoneNum)
+         
+         IOS（特殊处理）:
+         U3D->IOS：{"method":"ReqCallPhone","params":{"phoneNum":"123456789012"}}
+         
+         */
+        else if ([func isEqualToString:@"ReqCallPhone"]) {
+            if ([[dic allKeys] containsObject:@"phoneNum"]) {
+                NSString *phoneNum = [dic objectForKey:@"phoneNum"];
+                [[UIApplication sharedApplication]openURL:[NSURL URLWithString:[@"tel://" stringByAppendingString:phoneNum]]];
+            }
+        }
+        /**
+         打开智能导游窗口和关闭智能导游窗口（OnIntelligentFun是回调函数）
+         */
+        else if ([func isEqualToString:@"ReqIntelligentFun"]) {
+            [self sendMsg2UnityOfType:@"OnIntelligentFun" andValue:@"{\"params\":{}}"];
+        }
     }
-    return [PWU3DCodec U3DCodec:@"false"];
+    return [PWU3DCodec U3DCodec:@"true"];
 }
 @end
